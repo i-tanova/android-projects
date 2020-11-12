ce347a892142f9d27232d021c19e6befdb4168d8
ivana <ivana@justsplit.it>
2020-11-12 13:39:08 +0200

    Sunny app

--- sunny-app/app/src/main/java/com/tanovait/sunnyapp/ui/MainActivity.kt
+++ sunny-app/app/src/main/java/com/tanovait/sunnyapp/ui/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.tanovait.sunnyapp.R
 import com.tanovait.sunnyapp.api.APIClient
 import com.tanovait.sunnyapp.api.APIInterface
+import com.tanovait.sunnyapp.data.WeatherResponse
 import kotlinx.android.synthetic.main.activity_main2.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -22,6 +22,7 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        city_text.text = "Sofia"
 
 
         coroutineScope.launch(Dispatchers.IO) {
@@ -30,18 +31,38 @@ class MainActivity2 : AppCompatActivity() {
                 val forecastResponse = api?.getForecast("Sofia", "metric")
 
                 withContext(Dispatchers.Main) {
-                    weather_api_text.text = weatherResponse!!.weather[0].description
-                    val sb = StringBuffer()
-                    val format = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
-                    forecastResponse!!.list.forEach{
-                        sb.append(format.format(Date(it.dt * 1000))+ " " + it.weather[0].description)
-                        sb.appendLine()
-                    }
-                    forecast_api_text.text = sb.toString()
+                    val imageR = getWeatherImageR(weatherResponse)
+                    today_image.setImageDrawable(getDrawable(imageR))
+                    today_description.text = weatherResponse!!.weather[0].description
+                    today_temperature.text = "${weatherResponse!!.main.temp} C"
+
+//                    val sb = StringBuffer()
+//                    val format = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+//                    forecastResponse!!.list.forEach{
+//                        sb.append(format.format(Date(it.dt * 1000)) + " " + it.weather[0].description)
+//                        sb.appendLine()
+//                    }
+//                    forecast_api_text.text = sb.toString()
                 }
             }catch (e: Exception){
                 Log.e("TAG", e.localizedMessage, e)
             }
         }
+    }
+
+    //https://openweathermap.org/weather-conditions
+    private fun getWeatherImageR(weatherResponse: WeatherResponse?): Int {
+        var imageR = R.drawable.sun
+        weatherResponse!!.weather.forEach {
+            val iconCode = it.icon.dropLast(1)
+            if (iconCode in listOf<String>("13")) {
+                return R.drawable.snow
+            }
+
+            if (iconCode in listOf<String>("09", "10", "11")) {
+                imageR = R.drawable.rain
+            }
+        }
+        return imageR
     }
 }
